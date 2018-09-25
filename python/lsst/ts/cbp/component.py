137b4bf4d77dbc4b9af0f7e7cebdf5117d570c8e
Eric Coughlin <couger01@luther.edu>
2018-09-25 13:56:25 -0700

    Added in EUPS support & address and port arguments

--- python/lsst/ts/cbp/component.py
+++ python/lsst/ts/cbp/component.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 class CBPComponent:
-    def __init__(self):
+    def __init__(self,address: str, port: int):
         self.socket = None
         self.altitude = None
         self.azimuth = None
@@ -11,6 +11,8 @@ class CBPComponent:
         self.mask_rotation = None
         self.mask_dictionary = {}
         self.focus = None
+        self._address = address
+        self._port = port
         self.get_cbp_telemetry()
         self.panic_status = None
         self.check_panic_status()
@@ -24,7 +26,7 @@ class CBPComponent:
     def connect(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.settimeout(5)
-        self.socket.connect(("140.252.33.12", 5000))
+        self.socket.connect((self._address, self._port))
 
     def get_azimuth(self):
         self.connect()
@@ -33,7 +35,7 @@ class CBPComponent:
         self.azimuth = self.socket.recv(128).decode('ascii', 'ignore')
         self.socket.close()
 
-    def move_azimuth(self,position):
+    def move_azimuth(self,position: float):
         self.connect()
         self.socket.sendall("new_az={0:f}\r".format(position).encode('ascii'))
         reply = self.socket.recv(128).decode('ascii', 'ignore')
@@ -48,7 +50,7 @@ class CBPComponent:
         self.altitude = self.socket.recv(128).decode('ascii', 'ignore')
         self.socket.close()
 
-    def move_altitude(self,position):
+    def move_altitude(self,position: float):
         raise NotImplementedError
         self.connect()
         self.socket.sendall("new_alt{0:f}\r".format(position).encode('ascii'))
@@ -63,7 +65,7 @@ class CBPComponent:
         self.focus = self.socket.recv(128).decode('ascii')
         self.socket.close()
 
-    def change_focus(self,position):
+    def change_focus(self,position: int):
         self.connect()
         self.socket.sendall("new_foc={0:f}\r".format(position).encode('ascii'))
         reply = self.socket.recv(128).decode('ascii', 'ignore')
@@ -77,7 +79,7 @@ class CBPComponent:
         self.mask = self.socket.recv(128).decode('ascii')
         self.socket.close()
 
-    def set_mask(self, mask):
+    def set_mask(self, mask: str):
         raise NotImplementedError
         self.connect()
         self.socket.sendall("new_msk={0:f}".format(mask['id']).encode('ascii'))
@@ -92,7 +94,7 @@ class CBPComponent:
         self.mask_rotation = self.socket.recv(128).decode('ascii')
         self.socket.close()
 
-    def set_mask_rotation(self,mask_rotation):
+    def set_mask_rotation(self,mask_rotation: float):
         self.connect()
         self.socket.sendall("new_rot={0:f}".format(mask_rotation).encode('ascii'))
         reply = self.socket.recv(128).decode('ascii', 'ignore')
@@ -141,7 +143,7 @@ class CBPComponent:
 
 
 def main():
-    cbp = CBPComponent()
+    cbp = CBPComponent("140.252.33.12", 5000)
     cbp.move_azimuth(0)
     print("moved")
 
