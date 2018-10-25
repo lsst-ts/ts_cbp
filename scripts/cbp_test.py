88d4e3ae71f19b1a5b419e42a3274555dcdd7ad5
Eric Coughlin <couger01@luther.edu>
2018-10-25 12:56:45 -0700

    Fixed asyncio awaits

--- scripts/cbp_test.py
+++ scripts/cbp_test.py
@@ -15,55 +15,47 @@ class CBPRemote():
         standby_topic = self.remote.cmd_standby.DataType()
         standby_ack = await self.remote.cmd_standby.start(standby_topic,timeout=10)
         self.log.info(standby_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.standby())
 
     async def disable(self):
         disable_topic = self.remote.cmd_disable.DataType()
         disable_ack = await self.remote.cmd_disable.start(disable_topic,timeout=10)
         self.log.info(disable_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.disable())
 
     async def start(self):
         start_topic = self.remote.cmd_start.DataType()
         start_ack = await self.remote.cmd_start.start(start_topic,timeout=10)
         self.log.info(start_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.start())
 
     async def enable(self):
         enable_topic = self.remote.cmd_enable.DataType()
         enable_ack = await self.remote.cmd_enable.start(enable_topic,timeout=10)
         self.log.info(enable_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.enable())
 
     async def move_azimuth(self, azimuth):
         move_azimuth_topic = self.remote.cmd_moveAzimuth.DataType()
         move_azimuth_topic.azimuth = azimuth
         move_azimuth_ack = await self.remote.cmd_moveAzimuth.start(move_azimuth_topic,timeout=10)
         self.log.info(move_azimuth_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.move_azimuth(azimuth))
 
     async def move_altitude(self, altitude):
         move_altitude_topic = self.remote.cmd_moveAltitude.DataType()
         move_altitude_topic.altitude = altitude
         move_altitude_ack = await self.remote.cmd_moveAltitude.start(move_altitude_topic,timeout=10)
         self.log.info(move_altitude_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.move_altitude(altitude))
 
     async def set_focus(self,focus):
         set_focus_topic = self.remote.cmd_setFocus.DataType()
         set_focus_topic.focus = focus
         set_focus_ack = await self.remote.cmd_setFocus.start(set_focus_topic,timeout=10)
         self.log.info(set_focus_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.set_focus(focus))
 
     async def park(self):
         park_topic = self.remote.cmd_park.DataType()
         park_ack = await self.remote.cmd_park.start(park_topic,timeout=10)
         self.log.info(park_ack.ack.ack)
-        asyncio.get_event_loop().run_until_complete(self.park())
 
 
-def do_stuff():
+async def do_stuff():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
@@ -71,17 +63,20 @@ def do_stuff():
     logger.addHandler(ch)
     logger.info("Starting")
     cbp_remote = CBPRemote()
-    parser = argh.ArghParser()
-    parser.add_commands(
-        [cbp_remote.standby,
-         cbp_remote.start,
-         cbp_remote.disable,
-         cbp_remote.enable,
-         cbp_remote.move_altitude,
-         cbp_remote.move_azimuth,
-         cbp_remote.set_focus,
-         cbp_remote.park])
-    parser.dispatch()
+    await cbp_remote.start()
+    await cbp_remote.enable()
+    await cbp_remote.move_altitude(0)
+    # parser = argh.ArghParser()
+    # parser.add_commands(
+    #     [cbp_remote.standby,
+    #      cbp_remote.start,
+    #      cbp_remote.disable,
+    #      cbp_remote.enable,
+    #      cbp_remote.move_altitude,
+    #      cbp_remote.move_azimuth,
+    #      cbp_remote.set_focus,
+    #      cbp_remote.park])
+    # parser.dispatch()
 
 if __name__ == '__main__':
-    do_stuff()
+    asyncio.get_event_loop().run_until_complete(do_stuff())
